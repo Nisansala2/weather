47ad59611b301aa3345da6d8b48473ff18eb8cdb
Nisansala2 <nisansalagamchchige@gmail.com>
2024-07-04 10:02:15 +0530

    Create new repostry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Components/Weather.css';
+import Weather from './Components/Weather.js';
+import Search from './Components/Search.js';
+  
+ 
+
+  const App = () => {
+    const [weatherData, setWeatherData] = useState(null);
+  
+    const fetchWeather = async (city) => {
+      try {
+        const apiKey = process.env.d378ab9eebf51699bd8d20ecaaf78566;
+        if (!apiKey) {
+          throw new Error("API key is missing");
+        }
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}}&units=metric`);
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+  
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <Search fetchWeather={fetchWeather} />
+        {weatherData && <Weather data={weatherData} />}
+      </div>
+    );
+  };
+  
+ 
 
 export default App;
