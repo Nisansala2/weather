4b0d037b02e567d020f412efa07d9ee665ec5901
Nisansala2 <nisansalagamchchige@gmail.com>
2024-07-05 02:58:07 +0530

    change appear

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ function App() {
   const apiKey= 'd378ab9eebf51699bd8d20ecaaf78566'
   const [weatherData,setWeatherData] =useState([{}])
   const [city, setCity]= useState( "")
+  
 
-  const getWeather =(event) => {
-    if (event.key === "Enter") {
+  const handlekey =(event) => {
+    
+      switch (event.button) {
+        case 0:
+          console.log("Left button clicked");
+          break;
+        case 1:
+          console.log("Middle button clicked");
+          break;
+        case 2:
+          console.log("Right button clicked");
+          break;
+          default:
+            console.log("Unknown button clicked");
+      } {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(
       response => response.json()
       ).then(
@@ -20,37 +34,52 @@ function App() {
         }
       )
     }
+
   } 
+
   
     return (
       <div className='app'>
-       
-        <input  className='input'
-       placeholder='Enter city' 
+      <h2 className='welcome'>Welcome to weather app.</h2>
+      <p className='desp'> 
+       Enter in a city to get the weather</p>
+      
+       <div className='contain'>
+        <div className='input'>
+        <input placeholder='Enter city' 
         onChange={e => setCity(e.target.value)}
-        value={city}
-        onKeyPress={getWeather}></input>
+        value={city}></input>
+        </div>
+        <div className='button'>
+             <button onClick={handlekey}>Serach</button>
+    </div>
+    </div>
 
          {typeof weatherData.main ==='undefined' ? (
           <div>
-            <h2 className='welcome'>Welcome to weather app.</h2>
-            <p className='desp'> 
-             Enter in a city to get the weather</p>
+            
             <div className='cloud'>
             <img src='/images/sun.webp' alt= 'sun' style={{ height: '180px' } }/ >
            
             </div>
-            <button>Login</button>
+           
             </div>
         ):
         (
+          <div>
+          <h3 className='whead'>Weather in {weatherData.name}</h3>
           <div className='weather-data'>
-            
-          <h2 className='whead'>Weather in {weatherData.name}</h2>
-          <p className='temp'>Temperature: {weatherData.main.temp}°C</p>
+          
+            <div className='temp'>
+          
+          <p >Temperature  {weatherData.main.temp}°C</p>
+          </div>
+          <div className='text'>
           <p className='des'>Weather: {weatherData.weather[0].description}</p>
           <p className='hum'>Humidity: {weatherData.main.humidity}%</p>
           <p className='wind'>Wind Speed: {weatherData.wind.speed} m/s</p>
+          </div>
+        </div>
         </div>
         )}
         {weatherData.cod === "404" ?(
